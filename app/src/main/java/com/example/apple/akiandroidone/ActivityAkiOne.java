e340440aa479cfdc95a79dbdc1e6221b3fad6a43
Agila <agila2612@gmail.com>
2018-08-26 21:58:17 +0530

    first commit

---
+++ app/src/main/java/com/example/apple/akiandroidone/ActivityAkiOne.java
@@ -0,0 +1,25 @@
+package com.example.apple.akiandroidone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ActivityAkiOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.akilayout1);
+       final  EditText ed=findViewById(R.id.editText);
+        Button b=findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),akiactivity2.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/apple/akiandroidone/akiactivity2.java
@@ -0,0 +1,13 @@
+package com.example.apple.akiandroidone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class akiactivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akiactivity2);
+    }
+}
